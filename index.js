c461753ca2a200c17c661fad8d61edf0c5f51f60
Arpan Parikh <f2016082@pilani.bits-pilani.ac.in>
2018-06-18 14:55:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const fissionProvider = require(./provider/fissionProvider);
-const fissionDeploy = require(./deploy/fissionDeploy);
+const fissionProvider = require('./provider/fissionProvider');
+const fissionDeploy = require('./deploy/fissionDeploy');
 
 class fissionplugin {
   constructor(serverless, options) {
@@ -10,48 +10,8 @@ class fissionplugin {
 
     this.serverless.pluginManager.addPlugin(fissionProvider);
     this.serverless.pluginManager.addPlugin(fissionDeploy);
-    this.commands = {
-      welcome: {
-        usage: 'Helps you start your first Serverless plugin',
-        lifecycleEvents: [
-          'hello',
-          'world',
-        ],
-        options: {
-          message: {
-            usage:
-              'Specify the message you want to deploy '
-              + '(e.g. "--message \'My Message\'" or "-m \'My Message\'")',
-            required: true,
-            shortcut: 'm',
-          },
-        },
-      },
-    };
-
-    this.hooks = {
-      'before:welcome:hello': this.beforeWelcome.bind(this),
-      'welcome:hello': this.welcomeUser.bind(this),
-      'welcome:world': this.displayHelloMessage.bind(this),
-      'after:welcome:world': this.afterHelloWorld.bind(this),
-    };
   }
 
-  beforeWelcome() {
-    this.serverless.cli.log('Hello from Serverless!');
-  }
-
-  welcomeUser() {
-    this.serverless.cli.log('Your message:');
-  }
-
-  displayHelloMessage() {
-    this.serverless.cli.log(`${this.options.message}`);
-  }
-
-  afterHelloWorld() {
-    this.serverless.cli.log('Please come again!');
-  }
 }
 
 module.exports = fissionplugin;
