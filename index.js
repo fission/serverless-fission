440fd6bb312e92bf11b14d97ee421303e37d68b0
Arpan Parikh <f2016082@pilani.bits-pilani.ac.in>
2018-07-09 12:58:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,16 +14,24 @@
 
 const fissionProvider = require('./provider/fissionProvider');
 const fissionDeploy = require('./deploy/fissionDeploy');
+const fissionCreate = require('./create/fissionCreate');
+const fissionInfo = require('./info/fissionInfo');
+const fissionInvoke = require('./invoke/fissionInvoke');
+const fissionRollback = require('./rollback/fissionRollback');
+const fissionPrint = require('./print/fissionPrint');
 
 class fissionplugin {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
 
-    this.serverless.pluginManager.addPlugin(fissionProvider);
-    this.serverless.pluginManager.addPlugin(fissionDeploy);
+   this.serverless.pluginManager.addPlugin(fissionProvider);
+   this.serverless.pluginManager.addPlugin(fissionDeploy);
+   this.serverless.pluginManager.addPlugin(fissionCreate);
+   this.serverless.pluginManager.addPlugin(fissionInvoke);
+   this.serverless.pluginManager.addPlugin(fissionPrint);
+   this.serverless.pluginManager.addPlugin(fissionInfo);
+   this.serverless.pluginManager.addPlugin(fissionRollback);
   }
-
 }
-
 module.exports = fissionplugin;
