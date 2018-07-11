3253ed8b9c980a726ba42cf7892a18eb38ae7977
Sanket Sudake <sanket@infracloud.io>
2018-07-11 18:35:03 +0530

    Few changes in sample script

--- fission-script.js
+++ fission-script.js
@@ -2,12 +2,15 @@
 //
 // Use a Custom Resource Definition to extend the Kubernetes API and the client.
 //
+var fs = require('fs');
+    path = require('path');
 const Client = require('kubernetes-client').Client;
 const config = require('kubernetes-client').config;
 
+
 async function log_resource(resource) {
-  	const resources = await resource;
-	for (var i=0;i<resources.length;i++) {
+    var resources = await resource
+    for (var i in resources) {
         item = resources[i]
         console.log(item);
     }
@@ -22,6 +25,17 @@ async function list_envs(client) {
     const envs = await client.apis['fission.io'].v1.namespaces('default').environments.get();
     return envs['body']['items'];
 }
+
+async function list_pkgs(client) {
+    const pkgs = await client.apis['fission.io'].v1.namespaces('default').packages.get();
+    return pkgs['body']['items'];
+}
+
+async function list_httptriggers(client) {
+    const pkgs = await client.apis['fission.io'].v1.namespaces('default').httptriggers.get();
+    return pkgs['body']['items'];
+}
+
 
 async function get_env(client) {
 }
@@ -57,74 +71,73 @@ async function create_env(client) {
 }
 
 async function create_func(client) {
- const fn = {
-    apiVersion: 'fission.io/v1',
-    kind: 'Function',
-    metadata: {
-        clusterName: '',
-        generation: 1,
-        name: 'test',
-        namespace: 'default',
-        selfLink: '/apis/fission.io/v1/namespaces/default/functions/test',
-        
-    },
-    spec: {
-        InvokeStrategy: {
-            ExecutionStrategy: ['poolmgr'],
-            StrategyType: 'execution'
+    const fn = {
+        apiVersion: 'fission.io/v1',
+        kind: 'Function',
+        metadata: {
+            clusterName: '',
+            name: 'test',
+            namespace: 'default',
         },
-        configmaps: null,
-        environment: {
-            name: 'node',
-            namespace: 'default'
-        },
-        package: {
-            packageref: []
-        },
-        resources: {
-            
-        },
-        secrets: null
-    }
-}
- const fns = await client.apis['fission.io'].v1.namespaces('default').functions.post({body: fn});
- console.log('Functions:', fns); 
-}
-async function list_pkgs(client) {
-	    const pkgs = await client.apis['fission.io'].v1.namespaces('default').packages.get();
-	    return pkgs['body']['items'];
-}
-async function create_pkgs(client){
-const pkg = {
-    apiVersion: 'fission.io/v1',
-    kind: 'Package',
-    metadata: {
-        clusterName: '',
-        creationTimestamp: '2018-07-11T09:46:27Z',
-        generation: 1,
-        name: 'test-js-yc0b',
-        namespace: 'default',
-        selfLink: '/apis/fission.io/v1/namespaces/default/packages/hello-js-yc0b',
-        uid: '0ded3709-84f0-11e8-8ee8-0800277294f5'
-    },
-    spec: {
-        deployment: {
-            checksum: {},
-            type: 'literal'
-        },
-        environment: {
-            name: 'node',
-            namespace: 'default'
-        },
-        source: {
-            checksum: {}
+        spec: {
+            InvokeStrategy: {
+                ExecutionStrategy: ['poolmgr'],
+                StrategyType: 'execution'
+            },
+            configmaps: null,
+            environment: {
+                name: 'node',
+                namespace: 'default'
+            },
+            package: {
+                packageref: []
+            },
+            resources: {
+            },
+            secrets: null
         }
-    },
+    }
+    const fns = await client.apis['fission.io'].v1.namespaces('default').functions.post({body: fn});
+    console.log('Functions:', fns);
 }
 
-const pkgs = await client.apis['fission.io'].v1.namespaces('default').packages.post({ body: pkg});
-console.log('Packages:', pkgs); 
+async function create_pkgs(client) {
+    const pkg = {
+        apiVersion: 'fission.io/v1',
+        kind: 'Package',
+        metadata: {
+            clusterName: '',
+            name: 'test-js-yc0q',
+            namespace: 'default'
+        },
+        spec: {
+            deployment: {
+                type: 'literal',
+                literal: 'Cm1vZHVsZS5leHBvcnRzID0gYXN5bmMgZnVuY3Rpb24oY29udGV4dCkgewogICAgcmV0dXJuIHsKICAgICAgICBzdGF0dXM6IDIwMCwKICAgICAgICBib2R5OiAiSGVsbG8sIHdvcmxkIVxuIgogICAgfTsKfQo=',
+                checksum: {}
+            },
+            environment: {
+                name: 'node',
+                namespace: 'default'
+            },
+           source: {
+                checksum: {}
+            }
+        }
+    }
+    const pkgs = await client.apis['fission.io'].v1.namespaces('default').packages.post({ body: pkg});
+    console.log('Packages:', pkgs);
 }
+
+function get_contents(filePath, cb) {
+    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+        if (err) {
+            console.log(err);
+        }
+        cb(data);
+    });
+}
+
 async function main() {
   try {
     const client = new Client({ config: config.fromKubeconfig(), version: '1.9' });
@@ -135,17 +148,28 @@ async function main() {
     const all = await client.apis['apiextensions.k8s.io'].v1beta1.customresourcedefinitions.get();
 
     for (var i in all['body']['items']) {
-      item = all['body']['items'][i]
-   //console.log(item);
-      client.addCustomResourceDefinition(item);
-      }
-//  create_func(client);
-  create_pkgs(client);
-  log_resource(list_pkgs(client));
+        item = all['body']['items'][i]
+        client.addCustomResourceDefinition(item);
+    }
+    /*
+    const filename = 'hello.js';
+    // if http or https download to temp dir
+    const filePath = path.join(__dirname, filename);
+    get_contents(filePath, function(data) {
+        const archive = {
+            type: 'literal',
+            literal: 'Cm1vZHVsZS5leHBvcnRzID0gYXN5bmMgZnVuY3Rpb24oY29udGV4dCkgewogICAgcmV0dXJuIHsKICAgICAgICBzdGF0dXM6IDIwMCwKICAgICAgICBib2R5OiAiSGVsbG8sIHdvcmxkIVxuIgogICAgfTsKfQo=',
+            checksum: {}
+        }
+        create_pkgs(client, archive);
+    });*/
+    create_pkgs(client);
+
+   // create_func(client);
+    log_resource(list_pkgs(client));
   } catch (err) {
     console.error('Error: ', err);
   }
 }
 
 main();
-
