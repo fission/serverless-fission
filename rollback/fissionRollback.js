6b103cd8ce1615d70cbcf41a9f8718fd42edeb2b
Arpan Parikh <f2016082@pilani.bits-pilani.ac.in>
2018-07-19 14:03:20 +0530

    Update fissionRollback.js

--- rollback/fissionRollback.js
+++ rollback/fissionRollback.js
@@ -13,7 +13,7 @@
 'use strict';
 const Client = require('kubernetes-client').Client;
 const config = require('kubernetes-client').config;
-var func = require('../common.js');
+let func = require('../common.js');
 const client = new Client({ config: config.fromKubeconfig(), version: '1.9' });
 class fissionRollback {
 	constructor(serverless,options) {
@@ -46,12 +46,12 @@ class fissionRollback {
 	async rollbackFunction() {
          const all = await client.apis['apiextensions.k8s.io'].v1beta1.customresourcedefinitions.get();
 
-                for (var i in all['body']['items']) {
-                    var item = all['body']['items'][i]
+                for (let i in all['body']['items']) {
+                    let item = all['body']['items'][i]
                      client.addCustomResourceDefinition(item);
                 }
-	var env_name = this.options.env;
-	var nmspace = this.options.nmspace;
+	const env_name = this.options.env;
+	const nmspace = this.options.nmspace;
 	func.delete_env(client,env_name,nmspace);
 	}
 }
